3b565bead2c2af2db8c615255345929c1c53ba11
chenhao <chenhao159482@gmail.com>
2018-04-22 21:09:21 +0800

    图像搜索

---
+++ templates/feature_extraction.py
@@ -0,0 +1,14 @@
+from feature_extraction import feature
+# import feature_extraction
+
+def testCase(img_path):
+    f = feature()
+    norm_feat = f.extract_feat (img_path)
+    print (norm_feat)
+    print (norm_feat.shape)
+    print(type(norm_feat))
+    return 0
+
+
+if __name__ == '__main__':
+    testCase("H:/datasets/testingset/19700102125648863.JPEG")
